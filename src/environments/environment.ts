4608b9ec0a959a6b7469df447e8bbadb2562f528
NikaEliashvili <nikaeliashvili18@outlook.com>
2024-01-29 02:20:55 +0400

    changed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: process.env['API_KEY'],
-    authDomain: process.env['AUTH_DOMAIN'],
-    projectId: process.env['PROJECT_ID'],
-    storageBucket: process.env['STORAGE_BUCKET'],
-    messagingSenderId: process.env['MESSAGE_SENDER_ID'],
-    appId: process.env['APP_ID'],
+    apiKey: `${process.env['API_KEY']}`,
+    authDomain: `${process.env['AUTH_DOMAIN']}`,
+    projectId: `${process.env['PROJECT_ID']}`,
+    storageBucket: `${process.env['STORAGE_BUCKET']}`,
+    messagingSenderId: `${process.env['MESSAGE_SENDER_ID']}`,
+    appId: `${process.env['APP_ID']}`,
   },
 };
